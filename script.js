57d6e87e88346570ab3342a270511154ed88c044
RiccardoBeca <riccardobeca@gmail.com>
2022-04-26 16:36:37 +0100

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+/* 
+CONSEGNA
+
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
+Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+**Consigli del giorno:**
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
+
+1. Genero un numero random creando una funzione e random math
+2. Creo un ciclo for per avere 5 numeri random
+3. Creo un array dove inserisco i 5 numeri random
+4. Creo un ciclo  while per creare  5 prompt per richiedere all'utente 5 numeri
+5. creo un array vuoto dove andro' a pushare i 5 numeri inseriti dall'uente 
+6. confronto i due array e controllo se i numeri inseriti dall'utente sono anche nell'array dei numeri generati randomicamente
+
+*/
